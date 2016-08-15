2eafc9bbb4069f181aa8c82d475cae28d7e2f4fc
gabethexton <gabethexton@gmail.com>
2016-08-15 14:48:15 -0600

    users.js framed out

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,6 @@
 
 var express = require('express');
 var router = express.Router();
-var knex = require('../db/knex');
-var bcrypt = require('bcrypt');
 var jwt = require('jsonwebtoken');
 
 
--- routes/users.js
+++ routes/users.js
@@ -3,18 +3,34 @@
 var express = require('express');
 var router = express.Router();
 var knex = require('../db/knex');
-var bcrypt = require('bcrypt');
 var jwt = require('jsonwebtoken');
 
 
-GET /users/:id/contacts -- Display all contacts
+// GET /users/:id/contacts -- Display all contacts
+router.get('users/:id/contacts', function (req, res, next) {
+    //show contacts list
+})
 
-POST /users/:id/contacts -- Create a new contact
 
-PUT/users/:id/contacts/:id -- Update a contact
+// POST /users/:id/contacts -- Create a new contact
+router.post('users/:id/contacts', function (req, res, next) {
+    //check for existing contact w/ same info
 
-DELETE/users/:id/contacts/:id -- Delete a contact
+    //show NEW contact form
 
+})
+
+
+// PUT/users/:id/contacts/:id -- Update a contact
+router.put('users/:id/contacts/:id', function (req, res, next) {
+    //show UPDATE contact form
+})
+
+
+//DELETE/users/:id/contacts/:id -- Delete a contact
+router.delete('users/:id/contacts/:id', function (req, res, next) {
+    //delete
+})
 
 
 module.exports = router;
