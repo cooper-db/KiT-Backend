63606acd3ee83a53edb3430d24705c200f02d8f8
Kristen Foster-Marks <Kristenfoster-marks@Kristens-Macbrook-Pro.local>
2016-08-16 11:04:57 -0700

    fixed seed id increment

--- seeds/users.js
+++ seeds/users.js
@@ -4,12 +4,13 @@ exports.seed = function(knex, Promise) {
   return knex('users').del()
     .then(function () {
       return Promise.all([
-        
+
         knex('users').insert({id: 1, username: 'Gabe', password: 1234}),
         knex('users').insert({id: 2, username: 'Ryan', password: 1234}),
         knex('users').insert({id: 3, username: 'Jimmy', password: 1234}),
         knex('users').insert({id: 4, username: 'Vy', password: 1234}),
-        knex('users').insert({id: 5, username: 'Kristen', password: 1234})
+        knex('users').insert({id: 5, username: 'Kristen', password: 1234}),
+        knex.raw('ALTER SEQUENCE users_id_seq RESTART WITH 6')
 
       ]);
     });
