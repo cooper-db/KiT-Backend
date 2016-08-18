fea5bc01b45be8649b0d440d2e03da5ee016f944
Kristen Foster-Marks <Kristenfoster-marks@Kristens-Macbrook-Pro.local>
2016-08-18 14:30:09 -0700

    commit

--- routes/users.js
+++ routes/users.js
@@ -72,7 +72,7 @@ router.post('/:id/contacts', function (req, res, next) {
 
 
 // PUT/users/:id/contacts/:id -- Update a contact
-router.put('/:id/contacts/:contactID', function (req, res, next) {
+router.put('users/:id/contacts/:contactID', function (req, res, next) {
     //test route
     // res.send('users/:id/contacts/:id PUT route hit successfully');
 
